bb14d6f68d99efc7b63122c9b6ab0845fb4f70aa
Sarylov <jalsan.work@yandex.ru>
2024-05-26 05:26:08 +0300

    fix build

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,6 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 export default createRouter({
   history: createWebHistory(),
-  routes: [
-    {
-      path: '/',
-      component: () =>
-        import(/* webpackChunkName: "home" */ '@/views/Index.vue'),
-    },
-  ],
-})
+  routes: [],
+});
