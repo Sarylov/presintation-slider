4fb33c58a0139beb6ec019b7a590e13f30d3e7bb
Sarylov <jalsan.work@yandex.ru>
2024-08-31 00:06:21 +0300

    add analytics

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
-import { createApp } from 'vue'
-import { createPinia }  from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from '@/App.vue'
-import router from '@/router'
-import '@/assets/styles/index.css'
+import App from "@/App.vue";
+import router from "@/router";
+import "@/assets/styles/index.css";
+import { inject } from "@vercel/analytics";
 
-const pinia = createPinia()
+inject();
 
-createApp(App)
-.use(router)
-.use(pinia)
-.mount('#app')
+const pinia = createPinia();
+
+createApp(App).use(router).use(pinia).mount("#app");
