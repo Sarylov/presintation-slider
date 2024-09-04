8a7ffd42a08289f9fd64b8836926ec9e6d7de15e
Sarylov <jalsan.work@yandex.ru>
2024-09-05 00:53:09 +0300

    chnage photos

--- src/App.vue
+++ src/App.vue
@@ -38,16 +38,16 @@
 
 <script>
 // Импортируем изображения
-import image1 from "@/assets/images/image1.png";
-import image2 from "@/assets/images/image2.png";
-import image3 from "@/assets/images/image3.png";
-import image4 from "@/assets/images/image4.png";
-import image5 from "@/assets/images/image5.png";
-import image6 from "@/assets/images/image6.png";
-import image7 from "@/assets/images/image7.png";
-import image8 from "@/assets/images/image8.png";
-import image9 from "@/assets/images/image9.png";
-import image10 from "@/assets/images/image10.png";
+import image1 from "@/assets/images/1.png";
+import image2 from "@/assets/images/2.png";
+import image3 from "@/assets/images/3.png";
+import image4 from "@/assets/images/4.png";
+import image5 from "@/assets/images/5.png";
+import image6 from "@/assets/images/6.png";
+import image7 from "@/assets/images/7.png";
+import image8 from "@/assets/images/8.png";
+import image9 from "@/assets/images/9.png";
+import image10 from "@/assets/images/10.png";
 
 export default {
   data() {
